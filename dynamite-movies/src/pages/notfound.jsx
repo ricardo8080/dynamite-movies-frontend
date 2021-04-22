663a9d95837246c9419d59f48dd19aea5eb3ab95
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-04-22 01:27:07 -0400

    UI of notfound added, made with own css file

--- dynamite-movies/src/pages/notfound.jsx
+++ dynamite-movies/src/pages/notfound.jsx
@@ -1,9 +1,14 @@
 /*index.jsx*/
 import React from "react"
+import './notfound.css';
 
 export const NotFound = () => {
     return (
-        <div>404 NOT FOUND</div>
+        <div class="container">
+            <h2>Oops! Page not found.</h2>
+            <h1>404</h1>
+            <h5>We can't find the page you're looking for.</h5>
+        </div>
     );
 };
 
