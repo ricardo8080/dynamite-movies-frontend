61a3425d6034305f88e15c59dcd6b7a0cb966d0c
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-04-25 21:38:19 -0400

    Little Changes in Main Page, add navbar

--- dynamite-movies/src/pages/mainpage.jsx
+++ dynamite-movies/src/pages/mainpage.jsx
@@ -1,18 +1,35 @@
-/*index.jsx*/
 import React from "react";
 import auth from '../AuthenthicationFiles/auth';
+import logo from "../images/logo.svg"
 
 export const MainPage = (props) => {
     console.log("a")
     return (
         <div> 
             <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/#">
-                    <img src="/logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top">
+                <div class="container-fluid">
+                    <a className="navbar-brand" href="/Main-Page">
+                    <img src={logo} alt='logo' width="30" height="24" className="d-inline-block align-text-top">
                     </img>
-                    Dynamite Movies
+                        Dynamite Movies
                     </a>
+                    <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <a class="navbar-brand" href="#">
+                    <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24"></img>
+                    </a>
+                    
+                    <button
+                        type="button" className="btn btn-outline-info"
+                        onClick={() => {
+                            auth.logout(()=>{
+                                props.history.push("/");
+                            });
+                        }}        
+                    >Logout
+                    </button> 
                 </div>
             </nav>
 
@@ -28,15 +45,6 @@ export const MainPage = (props) => {
             }}
             >Detailed Form
             </button>   
-            <button
-                type="button" className="btn btn-outline-info"
-                onClick={() => {
-                    auth.logout(()=>{
-                        props.history.push("/");
-                    });
-                }}        
-            >Logout
-            </button>
 
         </div>
     )
