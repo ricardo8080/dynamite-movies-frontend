03446e39f6e2eed9298fdb27d1b0e18c63bc01c6
anditavillarroel99 <anditavillarroel99@gmail.com>
2021-04-30 14:01:05 -0400

    Add react-bootstrap style

--- dynamite-movies/src/components/movieCard.jsx
+++ dynamite-movies/src/components/movieCard.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import ImageMovie from '../assets/images/default.png';
 import "./style.css";
+import { Card } from "react-bootstrap";
 class MovieCard extends Component {
   render() {
     return (
-      <div className="card shadow-lg rounded movie-card--content">
-        <img class="card-img-top" src={ImageMovie} alt="Card image cap"/>
-        <div class="card-body">
-          <h4 class="card-title">Name of Video</h4>
-          <h6>Date:</h6>
-          <h6>Country:</h6>
-        </div>
-      </div>
+      <Card className="movie-card--content">
+        <Card.Img variant="top" src={ImageMovie} />
+        <Card.Body className="d-flex flex-column">
+          <Card.Title className="movie-card--title">Name of Video</Card.Title>
+          <Card.Subtitle >Date:</Card.Subtitle>
+          <Card.Subtitle >Country:</Card.Subtitle>
+        </Card.Body>
+      </Card>
     );
   }
 }
