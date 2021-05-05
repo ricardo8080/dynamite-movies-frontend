ae6db6d5ffd63d1d8e3257d428f3689ba3d3b1df
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-05-05 02:17:15 -0400

    Revert "added imgs to navbar, created filter button, search from country and gender logic implemented, still not totally functional"
    This reverts commit 09f72b40081a3121121952e1f57ca3ad6a77ebae.

--- src/pages/mainpage.jsx
+++ src/pages/mainpage.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import logo from "../images/logo.svg"
+import { AuthButton } from '../AuthenthicationFiles/authFiles';
 import { Link } from "react-router-dom";
-import PersonalizedNavBar from '../components/personalizedNavBar'
-import FilterPage from '../components/filterPage'
 export const MainPage = () => {
     return (
             <div> 
-                <PersonalizedNavBar/>
-                <FilterPage />
-                
+                <nav className="navbar navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a className="navbar-brand" href="/Main-Page">
+                        <img src={logo} alt='logo' width="30" height="24" className="d-inline-block align-text-top">
+                        </img>
+                            Dynamite Movies
+                        </a>
+                        <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                        <a class="navbar-brand" href="/#">
+                        <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24"></img>
+                        </a>
+                        
+                        <AuthButton />
+                    </div>
+                </nav>
 
                 <button type="button" className="btn btn-outline-info" >
                     <Link to="/SearchResultsPage"  style={{textDecoration: 'none'}}>
