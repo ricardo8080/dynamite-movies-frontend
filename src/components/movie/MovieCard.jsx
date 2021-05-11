b3284c57b3b33dd7f47c9bdb3ae414215f7e935e
anditavillarroel99 <anditavillarroel99@gmail.com>
2021-05-11 12:09:55 -0400

    Change card style to accept backend data

--- src/components/movie/MovieCard.jsx
+++ src/components/movie/MovieCard.jsx
@@ -2,18 +2,63 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Card } from "react-bootstrap";
 import ImageMovie from "../../assets/images/default.png";
+import moment from 'moment';
 import "../style.css";
 class MovieCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDetails: false,
+    };
+    this.show = this.show.bind(this);
+  }
+  show(event) {
+    event.preventDefault();
+    this.setState({ showDetails: !this.state.showDetails });
+  }
+  changeDateFormat(date) {
+    return moment(date.toString().split('T')[0]).format('MMMM Do, YYYY');
+
+  }
+
+  showGenres(list) {
+    let text = "";
+    list.forEach((genre) => {
+      genre !== list[list.length - 1]
+        ? text += `${genre}, `
+        : text += `${genre}`;
+    });
+    return text;
+  }
   render() {
     const { movie } = this.props;
     return (
       <div>
-        <Card className="movie-card--content" key={movie._id}>
+        <Card className="movie-card--content" key={movie._id} onClick={(event) => this.show(event)}>
           <Card.Img variant="top" src={movie.principalImage} />
           <Card.Body className="d-flex flex-column">
             <Card.Title className="movie-card--title">{movie.nameMovie}</Card.Title>
-            <Card.Subtitle>Date: {movie.releaseDate}</Card.Subtitle>
-            <Card.Subtitle>Country: {movie.country}</Card.Subtitle>
+            <Card.Subtitle className="movie-card--subtitle">
+              <strong>Release Date: </strong>
+              {this.changeDateFormat(movie.releaseDate)}
+            </Card.Subtitle>
+            <Card.Subtitle className="movie-card--subtitle">
+              <strong>Country: </strong>
+              {movie.country}
+            </Card.Subtitle>
+            <Card.Subtitle className="movie-card--subtitle">
+              <strong>Genres: </strong>
+              {this.showGenres(movie.genres)}
+            </Card.Subtitle>
+            {this.state.showDetails
+              ? <>
+                <Card.Subtitle className="movie-card--subtitle">
+                  <strong>Description: </strong>
+                </Card.Subtitle>
+                <Card.Text className="movie-card--text">{movie.description}</Card.Text>
+              </>
+              : <></>
+            }
           </Card.Body>
         </Card>
       </div>
@@ -27,9 +72,11 @@ MovieCard.defaultProps = {
   movie: {
     _id: 0,
     nameMovie: "none",
-    releaseDate: "none",
+    releaseDate: new Date(),
     country: "none",
+    genres: [],
     principalImage: ImageMovie,
+    description: "none",
   },
 };
 export default MovieCard;
